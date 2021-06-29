1707dfa4e76e35719ef70f28112299a82747c68b
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-06-29 13:37:27 -1000

    adds Board Data logic into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
 import NewBoard from './components/NewBoard';
+import BoardList from './components/BoardList';
+import Board from './components/Board';
+import axios from 'axios';
+
 
 function App() {
+  // sets up state variables for Board component
+  const [boardData, setBoardData] = useState([]);
+  const [selectedBoard, setSelected] = useState({
+    title: '',
+    owner: '',
+    board_id: null
+  })
+  const selectBoard = (board) => {setSelected(board)};
+  const boardsElements = boardData.map(board => {
+    return (
+      <li><Board board={board} onBoardSelect={selectBoard} /></li>
+    )
+  })
+  const updateBoardData = (event) => {
+    setBoardData(event.target.value);
+  }
+
+  
   return (
-    <NewBoard />
-  )
+    <main>
+      <NewBoard />
+    </main>
+  );
 }
 
 export default App;
