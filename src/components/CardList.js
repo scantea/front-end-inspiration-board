b3ad836bfcefb8935b1f6118e211669c786d1270
scantea <71529653+scantea@users.noreply.github.com>
2021-07-01 17:28:14 -0500

    sorting cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,6 +5,7 @@ import NewCardForm from "./NewCard";
 
 const CardList = (props) => {
   const [cardData, setCardData] = useState([]);
+  const [sortType, setSortType] = useState("card_id");
 
   useEffect(() => {
     axios
@@ -12,13 +13,27 @@ const CardList = (props) => {
         `${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`
       )
       .then((response) => {
+        //insert sort code
+        const sortArray = (type) => {
+          const types = {
+            card_id: "card_id",
+            message: "message",
+            likes_count: "likes_count",
+          };
+        };
+        const sortProperty = types[type];
+        const sorted = [...CardList].sort(
+          (a, b) => b[sortProperty] - a[sortProperty]
+        );
+        setCardData(sorted);
         setCardData(response.data.cards);
-      })
+        sortArray(sortType);
+      }) //logic to sort cards; create a function in or out, and call it here
       .catch((error) => {
         console.log("Error:", error);
         alert("ooopsie Daisy, couldn't get cards on our board!! 😖 ");
       });
-  }, [props.board]);
+  }, [props.board, sortType]); //[props.board,sortType]
 
   //Delete Logic for a single card
   const deleteCard = (card) => {
@@ -80,6 +95,11 @@ const CardList = (props) => {
       });
   };
 
+  // const sort_cards = (card) =>{
+
+  //   return ()
+  // }
+
   return (
     <section className="cards__container">
       <section>
@@ -87,11 +107,34 @@ const CardList = (props) => {
         <div className="card-items__container">{cardElements}</div>
       </section>
       <NewCardForm postNewCard={postNewCard}></NewCardForm>
+      <section className="sort__selection__container">
+        <h3>Sort By:</h3>
+        <select onChange={(event) => setSortType(event.target.value)}>
+          <option value="card_id">ID</option>
+          <option value="message">Alphabetically</option>
+          <option value="likes_count">Number likes</option>
+        </select>
+      </section>
     </section>
   );
-  // const updateCardData = (event) => {
-  //     setCardData(event.target.value);
-  // }
 };
 
 export default CardList;
+
+// {
+//   "board_id":3,
+//   "cards":[
+//     {"board_id":3,"card_id":1,
+//     "likes_count":0,"message":"ffdfsdf"},
+//     {
+//       "board_id":3,"card_id":8,"likes_count":0,
+//       "message":"TESTING SLACKBOT 123"},
+//       {"board_id":3,"card_id":9,"likes_count":0,
+//       "message":"TESTING AGAIN"}],
+//       "owner":"fdasf","title":"dafdf"}
+
+// 1: {board_id: 3, card_id: 1,
+//  likes_count: 0, message: "ffdfsdf"}
+
+//select drop down to select (via weather report); grab the value, pass it into an argument that does
+//the sorting, sort by , could potentially do it on the backend; sort by field
